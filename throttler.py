db71badb83b5fad5e59d18563693ececa237a77e
Kai Yang <kfstorm@outlook.com>
2024-02-26 13:50:19 +0800

    per-host throttler

--- throttler.py
+++ throttler.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import time
 import httpx
 
@@ -6,24 +7,23 @@ from config import app_config
 
 class Throttler:
     def __init__(self):
-        self.next_call_time = 0
+        self.next_call_time = defaultdict(float)
 
     async def _on_request(self, request: httpx.Request):
-        if "douban" in request.url.host:
-            now = time.time()
-            if now < self.next_call_time:
-                raise Exception(
-                    "Rate limited. Need to wait at least "
-                    + str(self.next_call_time - now)
-                    + " seconds before the next call."
-                )
+        next_call_time = self.next_call_time[request.url.host]
+        now = time.time()
+        if now < next_call_time:
+            raise Exception(
+                f"Rate limited. Need to wait at least {next_call_time - now}"
+                + f" seconds before the next call to {request.url.host}."
+            )
 
     async def _on_response(self, response: httpx.Response):
         if response.status_code == 302 and "sec.douban.com" in response.headers.get(
             "location"
         ):
             # Rate limited.
-            self.next_call_time = (
+            self.next_call_time[response.url.host] = (
                 time.time() + app_config.douban_rate_limit_delay_seconds
             )
             raise Exception(
@@ -43,8 +43,11 @@ class Throttler:
     def get_info(self):
         now = time.time()
         return {
-            "is_rate_limited": now < self.next_call_time,
-            "wait_time": max(0, self.next_call_time - now),
+            key: {
+                "is_rate_limited": now < value,
+                "wait_time": max(0, value - now),
+            }
+            for key, value in self.next_call_time.items()
         }
 
 
